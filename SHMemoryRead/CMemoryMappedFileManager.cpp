04c1740961deefa40f8b78509d1cad8275a4cd01
yuji-yamamoto99 <yuji-yamamoto99@outlook.jp>
2020-05-31 00:11:21 +0900

    newproject

--- SHMemoryRead/CMemoryMappedFileManager.cpp
+++ SHMemoryRead/CMemoryMappedFileManager.cpp
@@ -1,12 +1,100 @@
 #include "stdafx.h"
 #include "CMemoryMappedFileManager.h"
 
-
-CMemoryMappedFileManager::CMemoryMappedFileManager()
+//コンストラクタ
+CMemoryMappedFileManager::CMemoryMappedFileManager(int nSize, LPCTSTR lpName)
 {
+	//
+	m_hShareMemory = NULL;
+	//
+	m_lpzName = _T("");
+
+	//
+	Create(nSize,lpName);
+
 }
 
-
+//デストラクタ
 CMemoryMappedFileManager::~CMemoryMappedFileManager()
 {
+
+	Close();
+
+}
+
+//共有メモリ生成
+bool CMemoryMappedFileManager::Create( int nSize , LPCTSTR lpName) {
+
+	if ( m_hShareMemory != NULL || lpName == _T("") ) {
+
+		return false;
+
+	}
+
+	//
+	m_lpzName = lpName;
+	m_nSize = nSize;
+
+	HANDLE hHandle = CreateFileMapping(NULL, NULL, PAGE_READWRITE, NULL, nSize, lpName);
+	if (!hHandle) {
+
+		//Fail to Createfilemapping Object?
+		return false;
+	}
+
+	m_hShareMemory = hHandle;
+
+	return true;
+
+}
+
+void CMemoryMappedFileManager::Read(LPVOID lpReadValue) {
+
+	
+	if (!m_hShareMemory) {
+
+		//⇒なければ作る
+		if (!Create(m_nSize, m_lpzName)) {
+
+			//⇒失敗
+			return;
+		}
+
+	}
+
+	//共有メモリから値取得
+	lpReadValue = (int*)MapViewOfFile(m_hShareMemory,FILE_MAP_ALL_ACCESS,NULL,NULL,m_nSize);
+	if (lpReadValue != NULL) {
+
+		m_pMapView = lpReadValue;
+	}
+
+}
+
+void CMemoryMappedFileManager::Write() {
+
+	if (!m_hShareMemory) {
+
+		return;
+
+	}
+	
+
+
+}
+
+//破棄
+void CMemoryMappedFileManager::Close()
+{
+	
+	//
+	UnmapViewOfFile(m_pMapView);
+	
+	m_pMapView = NULL;
+
+	//
+	CloseHandle(m_hShareMemory);
+
+	m_hShareMemory = NULL;
+
 }
--- SHMemoryRead/CMemoryMappedFileManager.h
+++ SHMemoryRead/CMemoryMappedFileManager.h
@@ -2,12 +2,34 @@
 class CMemoryMappedFileManager
 {
 public:
-	CMemoryMappedFileManager();
+	CMemoryMappedFileManager(int nSize, LPCTSTR lpName);
 	virtual ~CMemoryMappedFileManager();
 
 
+public:
+
+	bool Create( int nSize ,LPCTSTR lpName);
+
+	void Read(LPVOID lpReadValue);
+
+	void Write();
+
+	void Close();
 
 
+protected:
+
+	//
+	HANDLE m_hShareMemory;
+
+	//
+	LPCTSTR m_lpzName;
+
+	//
+	int m_nSize;
+
+	//
+	LPVOID	m_pMapView;
 
 };
 
